a3fcd86b3c1b4e07364f6ec8ca3ddf4d22915a3b
Miina Lervik <hallaelsa@hotmail.com>
2017-11-06 20:35:36 +0100

    prøver meg på redux-persist. Prøvde å lage apk, men den henger seg...

--- App.js
+++ App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
-import Reducer from './src/Reducer';
+import reducers from './src/Reducer';
 import List from "./src/List";
 import Add from './src/Edit';
-import Test from "./src/test";
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
 
 const AppNavigation = StackNavigator({
   Home: { screen: List },
   Edit: { screen: Add },
 });
 
+const store = createStore(reducers);
+const persistor = persistStore(store);
+
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(Reducer)}>
+      <Provider store={store}>
         <AppNavigation />
       </Provider>
     );
